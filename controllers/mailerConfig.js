d4d80f291630907c9d2bcfc7050238931f95035e
Gleb Timoshenko <timoshenkoglebas@ya.ru>
2023-10-27 08:22:52 +0300

    nodemailer fix

--- controllers/mailerConfig.js
+++ controllers/mailerConfig.js
@@ -3,5 +3,5 @@ module.exports = {
 	clientId: '841009556397-rvihmc64ruim7lrtm8rqo2a6qrmlrqmq.apps.googleusercontent.com',
 	clientSecret: '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',
 	refreshToken: '1//04pPPqnvT1qzYCgYIARAAGAQSNwF-L9IrQ3t0O53Fys1fE5P3KQCXbnrmiYglaQiVWHzL9h2ztKVU49b2F3OYZoG-5LbxwLRCuJc',
-	password: 'ZTaen93igarPQn0rHmpF'
+	password: '6yCuu5k9NfeVvj37xXXk'
 }
